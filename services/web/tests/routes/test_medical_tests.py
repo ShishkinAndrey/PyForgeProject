7714f44257db3d12fa7d82ae293270b552e74aee
andrey.shishkin <andrey.shishkin@quantori.com>
2022-03-23 14:53:06 +0300

    test_medical_tests.py corrections

--- services/web/tests/routes/test_medical_tests.py
+++ services/web/tests/routes/test_medical_tests.py
@@ -2,7 +2,7 @@ from tests.conftest import force_login
 
 
 @force_login('customer')
-def test_get_medical_tests(client, medical_tests):
+def test_get_medical_tests_by_customer(client, customer, medical_tests):
     response = client.get(
         'medical_tests/',
     )
@@ -42,8 +42,26 @@ def test_get_medical_tests(client, medical_tests):
     ]}
 
 
+@force_login('doctor')
+def test_get_medical_tests_by_doctor(client, doctor, medical_tests):
+    response = client.get(
+        'medical_tests/',
+    )
+
+    assert response.status_code == 403
+
+
+@force_login('assistant')
+def test_get_medical_tests_by_doctor(client, assistant, medical_tests):
+    response = client.get(
+        'medical_tests/',
+    )
+
+    assert response.status_code == 403
+
+
 @force_login('customer')
-def test_get_medical_tests_by_id(client, medical_tests):
+def test_get_medical_tests_by_id(client, customer, medical_tests):
     response = client.get(
         'medical_tests/1',
     )
@@ -62,9 +80,27 @@ def test_get_medical_tests_by_id(client, medical_tests):
 
 
 @force_login('customer')
-def test_get_medical_tests_by_incorrect_id(client, medical_tests):
+def test_get_medical_tests_by_incorrect_id(client, customer, medical_tests):
     response = client.get(
         'medical_tests/100',
     )
 
     assert response.status_code == 404
+
+
+@force_login('doctor')
+def test_get_medical_tests_by_id_by_doctor(client, doctor, medical_tests):
+    response = client.get(
+        'medical_tests/1',
+    )
+
+    assert response.status_code == 403
+
+
+@force_login('assistant')
+def test_get_medical_tests_by_id_by_doctor(client, assistant, medical_tests):
+    response = client.get(
+        'medical_tests/1',
+    )
+
+    assert response.status_code == 403
